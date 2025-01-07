62b87273ec9c269da79081b8c51b32f0934df024
hoangtunggg <tungthkk1@gmail.com>
2025-01-07 17:42:53 +0700

    tao entity user va role

---
+++ src/main/java/com/javaweb/repository/entity/RoleEntity.java
@@ -0,0 +1,22 @@
+package com.javaweb.repository.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "role")
+public class RoleEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "name", nullable = false)
+	private String name;
+	
+	@Column(name = "code", unique = true, nullable = false)
+	private String code;
+}
